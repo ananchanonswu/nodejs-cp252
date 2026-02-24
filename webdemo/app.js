5df9bc5d9ae045fb0b68d66a5cdf494f89cf3b4f
AnanchanonSWU <ananchanon.pansuwan@g.swu.ac.th>
2026-02-24 05:08:30 +0000

    upd

---
+++ webdemo/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const expenseController = require('./controllers/expenseController');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', expenseController.index);
+app.post('/add', expenseController.add);
+
+/**
+ * Server start at port 3000
+ * @param {date} title - The title of the server.
+ * @param {float} author - The author of the my author.
+ * @param {float} expense - Total spending.
+ * @param {text} detail - Additional details.
+ */
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
